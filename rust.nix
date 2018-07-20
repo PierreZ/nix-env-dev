fb513d79d3d5fd89c9c8c2c84e7ff5fced7549ed
Pierre Zemb <contact@pierrezemb.fr>
2018-07-21 00:17:06 +0200

    feat(rust): rls

--- rust.nix
+++ rust.nix
@@ -1,14 +1,12 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
 in
   with nixpkgs;
   stdenv.mkDerivation {
     name = "rust";
-    buildInputs = [
-	openssl
-      nixpkgs.latest.rustChannels.stable.rust
-      ];
+    buildInputs = [ openssl rustup ruststable cmake zlib ];
 
     shellHook = ''
         export OPENSSL_DIR="${openssl.dev}"
